bc928fb87a4ad8d5fdd8eb29d6ffeff00b221a8e
Mark Braspenning <mark.braspenning@gmail.com>
2013-07-11 00:45:07 +0200

    Initial commit with basic rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoChunks::Application.config.secret_key_base = 'a4602f5ecd4d25c69103df73c764f2a94cb5ee08c543e6856f5bc975858a692364e4affc8421b0c718cf6ce1673c93d58f46257c1aba736611f929695943317d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoChunks::Application.config.session_store :cookie_store, key: '_todo_chunks_session'
