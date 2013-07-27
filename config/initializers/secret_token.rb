3a11a04460fa8e3a052ac0cb797a5f07d652cc72
Mark Braspenning <mark.braspenning@gmail.com>
2013-07-28 01:32:48 +0200

    Started from railstutorial.org

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TodoChunks::Application.config.secret_key_base = 'a4602f5ecd4d25c69103df73c764f2a94cb5ee08c543e6856f5bc975858a692364e4affc8421b0c718cf6ce1673c93d58f46257c1aba736611f929695943317d'
+# TodoChunks::Application.config.secret_key_base = 'a4602f5ecd4d25c69103df73c764f2a94cb5ee08c543e6856f5bc975858a692364e4affc8421b0c718cf6ce1673c93d58f46257c1aba736611f929695943317d'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TodoChunks::Application.config.secret_key_base = secure_token
