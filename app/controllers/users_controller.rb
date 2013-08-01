67cbc5108a8d359641a0202b8d011764c92774fc
Mark Braspenning <mark.braspenning@gmail.com>
2013-08-01 12:31:21 +0200

    Fixed the bug with the signup page on first entry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 	
+	def new
+		@user = User.new
+	end
+
+	def index
+			redirect_to root_path
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
